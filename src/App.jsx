8c7f98344d166cd5762383a08aac4516433f61bc
Chinedu Nwogbo <chinedunw2@gmail.com>
2025-06-07 11:23:23 +0100

    error fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Apptheme } from './theme';
 import { GlobalStyles } from './styles/GlobalStyles';
